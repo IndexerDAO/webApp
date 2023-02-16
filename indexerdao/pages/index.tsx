ed7101eaee6555b36b2fa5039a6036cbaad2f48b
dylviz <dylanvizcarra@gmail.com>
2023-02-16 00:07:58 -0800

    Removed ScaffoldEth for now - Vercel doesn't like it

---
+++ indexerdao/pages/index.tsx
@@ -0,0 +1,96 @@
+import Head from "next/head";
+import Image from "next/image";
+import { Inter } from "@next/font/google";
+import styles from "@/styles/Home.module.css";
+import { Container, Divider, Flex } from "@chakra-ui/react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Learning_Advertisement from "./components/Learning_Advertisement";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+    return (
+        <>
+            <Head>
+                <title>Indexer DAO</title>
+                <meta
+                    name="description"
+                    content="An Indexer's Source for Learning"
+                />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1"
+                />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <Header />
+            <Learning_Advertisement />
+
+            <Flex align="center">
+                <div className={styles.grid}>
+                    <a
+                        href="#"
+                        className={styles.card}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <h2 className={inter.className}>
+                            Docs <span>-&gt;</span>
+                        </h2>
+                        <p className={inter.className}>
+                            Find in-depth information about Indexing
+                        </p>
+                    </a>
+
+                    <a
+                        href="#"
+                        className={styles.card}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <h2 className={inter.className}>
+                            Learn <span>-&gt;</span>
+                        </h2>
+                        <p className={inter.className}>
+                            Learn about Indexing in our interactive course
+                            with&nbsp;quizzes!
+                        </p>
+                    </a>
+
+                    <a
+                        href="#"
+                        className={styles.card}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <h2 className={inter.className}>
+                            Get Started <span>-&gt;</span>
+                        </h2>
+                        <p className={inter.className}>
+                            Discover new subraphs that need to be indexed.
+                        </p>
+                    </a>
+
+                    <a
+                        href="#"
+                        className={styles.card}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <h2 className={inter.className}>
+                            Deploy <span>-&gt;</span>
+                        </h2>
+                        <p className={inter.className}>
+                            Learn to deploy hardware optimized for indexing
+                        </p>
+                    </a>
+                </div>
+            </Flex>
+
+
+            <Footer />
+        </>
+    );
+}
