f1397a8d2f7006e5d18185c2761dae1bf44f7bbd
dylviz <dylanvizcarra@gmail.com>
2023-05-01 12:07:34 -0700

    Updated links

--- indexerdao/components/Header.tsx
+++ indexerdao/components/Header.tsx
@@ -1,144 +1,144 @@
-import { ReactNode } from 'react';
+import { ReactNode } from "react";
 import {
-  Box,
-  Flex,
-  Avatar,
-  HStack,
-  Link,
-  IconButton,
-  Text,
-  Menu,
-  MenuButton,
-  MenuList,
-  MenuItem,
-  MenuDivider,
-  useDisclosure,
-  useColorModeValue,
-  Stack,
-  Image,
-  useColorMode,
-  VStack,
-  LightMode,
-} from '@chakra-ui/react';
-import { HamburgerIcon, CloseIcon, AddIcon, MoonIcon } from '@chakra-ui/icons';
-import { FiChevronDown } from 'react-icons/fi';
+	Box,
+	Flex,
+	Avatar,
+	HStack,
+	Link,
+	IconButton,
+	Text,
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	MenuDivider,
+	useDisclosure,
+	useColorModeValue,
+	Stack,
+	Image,
+	useColorMode,
+	VStack,
+	LightMode,
+} from "@chakra-ui/react";
+import { HamburgerIcon, CloseIcon, AddIcon, MoonIcon } from "@chakra-ui/icons";
+import { FiChevronDown } from "react-icons/fi";
 
 const Links: NavLink_i[] = [
-  { disp: 'Home', linkto: '/' },
-  { disp: 'Learn', linkto: 'https://indexdao.gitbook.io/indexerdao/' },
-  { disp: 'About', linkto: '/about' },
+	{ disp: "Home", linkto: "/" },
+	{ disp: "Docs", linkto: "https://docs.gitbook.io/indexerdao/" },
+	{ disp: "About", linkto: "/about" },
 ];
 
 interface NavLink_i {
-  disp: string;
-  linkto: string;
+	disp: string;
+	linkto: string;
 }
 
 const NavLink = ({ disp, linkto }: NavLink_i) => (
-  <Link
-    px={2}
-    py={1}
-    rounded={'md'}
-    color={'gray.50'}
-    _hover={{
-      textDecoration: 'none',
-      color: 'black',
-      bg: useColorModeValue('gray.200', 'gray.700'),
-    }}
-    href={linkto}
-  >
-    {disp}
-  </Link>
+	<Link
+		px={2}
+		py={1}
+		rounded={"md"}
+		color={"gray.50"}
+		_hover={{
+			textDecoration: "none",
+			color: "black",
+			bg: useColorModeValue("gray.200", "gray.700"),
+		}}
+		href={linkto}
+	>
+		{disp}
+	</Link>
 );
 
 interface Props {
-  showLogin?: Boolean;
+	showLogin?: Boolean;
 }
 
 export default function Header(props: Props) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const { colorMode, toggleColorMode } = useColorMode();
-  const color1 = useColorModeValue('white', 'gray.900');
-  const color2 = useColorModeValue('gray.200', 'gray.700');
+	const { isOpen, onOpen, onClose } = useDisclosure();
+	const { colorMode, toggleColorMode } = useColorMode();
+	const color1 = useColorModeValue("white", "gray.900");
+	const color2 = useColorModeValue("gray.200", "gray.700");
 
-  return (
-    <>
-      <Box bg={'#1A365D'} px={10}>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <IconButton
-            size={'md'}
-            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
-            aria-label={'Open Menu'}
-            display={{ md: 'none' }}
-            onClick={isOpen ? onClose : onOpen}
-          />
-          <HStack spacing={8} alignItems={'center'}>
-            <Link href="/">
-              <Image
-                alt={'Logo'}
-                fit={'cover'}
-                align={'center'}
-                w={'240px'}
-                h={'100%'}
-                p={1}
-                src={'/assets/ID_logo2.png'}
-              />
-            </Link>
-          </HStack>
+	return (
+		<>
+			<Box bg={"#1A365D"} px={10}>
+				<Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
+					<IconButton
+						size={"md"}
+						icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+						aria-label={"Open Menu"}
+						display={{ md: "none" }}
+						onClick={isOpen ? onClose : onOpen}
+					/>
+					<HStack spacing={8} alignItems={"center"}>
+						<Link href="/">
+							<Image
+								alt={"Logo"}
+								fit={"cover"}
+								align={"center"}
+								w={"240px"}
+								h={"100%"}
+								p={1}
+								src={"/assets/ID_logo2.png"}
+							/>
+						</Link>
+					</HStack>
 
-          <Flex alignItems={'center'}>
-            <HStack
-              as={'nav'}
-              spacing={4}
-              display={{ base: 'none', md: 'flex' }}
-            >
-              <LightMode>
-                {Links.map((link) => (
-                  <NavLink
-                    key={link.disp}
-                    disp={link.disp}
-                    linkto={link.linkto}
-                  />
-                ))}
-              </LightMode>
+					<Flex alignItems={"center"}>
+						<HStack
+							as={"nav"}
+							spacing={4}
+							display={{ base: "none", md: "flex" }}
+						>
+							<LightMode>
+								{Links.map((link) => (
+									<NavLink
+										key={link.disp}
+										disp={link.disp}
+										linkto={link.linkto}
+									/>
+								))}
+							</LightMode>
 
-              <IconButton
-                variant={'ghost'}
-                aria-label="Toggle Dark Mode"
-                icon={<MoonIcon />}
-                color={useColorModeValue('gray.200', '#B5E853')}
-                onClick={toggleColorMode}
-              />
+							<IconButton
+								variant={"ghost"}
+								aria-label="Toggle Dark Mode"
+								icon={<MoonIcon />}
+								color={useColorModeValue("gray.200", "#B5E853")}
+								onClick={toggleColorMode}
+							/>
 
-              {props.showLogin && (
-                <Flex alignItems={'center'}>
-                  <Menu>
-                    <MenuButton
-                      py={2}
-                      transition="all 0.3s"
-                      _focus={{ boxShadow: 'none' }}
-                    >
-                      <HStack>
-                        <Avatar
-                          size={'sm'}
-                          src={'/assets/teamprofile/BrainFried.jpg'}
-                        />
-                        <Box display={{ base: 'none', md: 'flex' }}>
-                          <FiChevronDown />
-                        </Box>
-                      </HStack>
-                    </MenuButton>
-                    <MenuList bg={color1} borderColor={color2}>
-                      <MenuItem>Profile</MenuItem>
-                      <MenuDivider />
-                      <MenuItem>Sign out</MenuItem>
-                    </MenuList>
-                  </Menu>
-                </Flex>
-              )}
-            </HStack>
+							{props.showLogin && (
+								<Flex alignItems={"center"}>
+									<Menu>
+										<MenuButton
+											py={2}
+											transition="all 0.3s"
+											_focus={{ boxShadow: "none" }}
+										>
+											<HStack>
+												<Avatar
+													size={"sm"}
+													src={"/assets/teamprofile/BrainFried.jpg"}
+												/>
+												<Box display={{ base: "none", md: "flex" }}>
+													<FiChevronDown />
+												</Box>
+											</HStack>
+										</MenuButton>
+										<MenuList bg={color1} borderColor={color2}>
+											<MenuItem>Profile</MenuItem>
+											<MenuDivider />
+											<MenuItem>Sign out</MenuItem>
+										</MenuList>
+									</Menu>
+								</Flex>
+							)}
+						</HStack>
 
-            {/* <Button
+						{/* <Button
                             variant={"solid"}
                             colorScheme={"teal"}
                             size={"sm"}
@@ -147,7 +147,7 @@ export default function Header(props: Props) {
                         >
                             Action
                         </Button> */}
-            {/* <Menu>
+						{/* <Menu>
               <MenuButton
                 as={Button}
                 rounded={'full'}
@@ -168,23 +168,23 @@ export default function Header(props: Props) {
                 <MenuItem>Link 3</MenuItem>
               </MenuList>
             </Menu> */}
-          </Flex>
-        </Flex>
+					</Flex>
+				</Flex>
 
-        {isOpen ? (
-          <Box pb={4} display={{ md: 'none' }}>
-            <Stack as={'nav'} spacing={4}>
-              {Links.map((link) => (
-                <NavLink
-                  key={link.disp}
-                  disp={link.disp}
-                  linkto={link.linkto}
-                />
-              ))}
-            </Stack>
-          </Box>
-        ) : null}
-      </Box>
-    </>
-  );
+				{isOpen ? (
+					<Box pb={4} display={{ md: "none" }}>
+						<Stack as={"nav"} spacing={4}>
+							{Links.map((link) => (
+								<NavLink
+									key={link.disp}
+									disp={link.disp}
+									linkto={link.linkto}
+								/>
+							))}
+						</Stack>
+					</Box>
+				) : null}
+			</Box>
+		</>
+	);
 }
--- indexerdao/pages/index.tsx
+++ indexerdao/pages/index.tsx
@@ -1,98 +1,98 @@
-import Head from 'next/head';
-import styles from '@/styles/Home.module.css';
+import Head from "next/head";
+import styles from "@/styles/Home.module.css";
 import {
-  Container,
-  Divider,
-  Flex,
-  VStack,
-  Text,
-  HStack,
-  Box,
-  Image,
-  Stack,
-  useColorModeValue,
-} from '@chakra-ui/react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import Learning_Advertisement from '../components/Learning_Advertisement';
+	Container,
+	Divider,
+	Flex,
+	VStack,
+	Text,
+	HStack,
+	Box,
+	Image,
+	Stack,
+	useColorModeValue,
+} from "@chakra-ui/react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Learning_Advertisement from "../components/Learning_Advertisement";
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>Indexer DAO</title>
-        <meta name="description" content="An Indexer's Source for Learning" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<>
+			<Head>
+				<title>Indexer DAO</title>
+				<meta name="description" content="An Indexer's Source for Learning" />
+				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
 
-      <Header />
-      <Learning_Advertisement />
+			<Header />
+			<Learning_Advertisement />
 
-      <Container maxW={'5xl'} alignContent="center" pb={2} mb={6}>
-        <Flex justify="center">
-          <div className={styles.grid}>
-            <a
-              href="https://indexdao.gitbook.io/indexerdao/"
-              className={styles.card}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2>
-                Docs <span>-&gt;</span>
-              </h2>
-              <p>Find in-depth information about Indexing</p>
-            </a>
-            <a
-              href="https://indexing101.indexerdao.com/"
-              className={styles.card}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h2>
-                Learn <span>-&gt;</span>
-              </h2>
-              <p>
-                Learn about Indexing in our interactive course
-                with&nbsp;quizzes!
-              </p>
-            </a>
-          </div>
-        </Flex>
-      </Container>
+			<Container maxW={"5xl"} alignContent="center" pb={2} mb={6}>
+				<Flex justify="center">
+					<div className={styles.grid}>
+						<a
+							href="https://docs.indexerdao.com/"
+							className={styles.card}
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<h2>
+								Docs <span>-&gt;</span>
+							</h2>
+							<p>Find in-depth information about Indexing</p>
+						</a>
+						<a
+							href="https://docs.indexerdao.com/indexing-101/"
+							className={styles.card}
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<h2>
+								Indexing 101 <span>-&gt;</span>
+							</h2>
+							<p>
+								Learn about Indexing in our interactive course
+								with&nbsp;quizzes!
+							</p>
+						</a>
+					</div>
+				</Flex>
+			</Container>
 
-      <Divider color={'gray.300'} />
+			<Divider color={"gray.300"} />
 
-      <VStack bg={'gray.100'} justifyContent={'center'} p={5}>
-        <Text
-          color={useColorModeValue('gray.700', 'blackAlpha.700')}
-          fontWeight={600}
-          fontSize={'lg'}
-          p={4}
-          mb={2}
-        >
-          Supported By:
-        </Text>
-        <HStack>
-          <Stack align={'center'} w={300}>
-            <Flex
-              w={40}
-              h={40}
-              color={'white'}
-              rounded={'full'}
-              overflow="hidden"
-              bg={'gray.100'}
-              p={2}
-              pb={0}
-            >
-              <Image src="assets/TheGraphSymbol.png" />
-            </Flex>
-            <Text fontWeight={300}>Graph Advocates</Text>
-          </Stack>
-        </HStack>
-      </VStack>
+			<VStack bg={"gray.100"} justifyContent={"center"} p={5}>
+				<Text
+					color={useColorModeValue("gray.700", "blackAlpha.700")}
+					fontWeight={600}
+					fontSize={"lg"}
+					p={4}
+					mb={2}
+				>
+					Supported By:
+				</Text>
+				<HStack>
+					<Stack align={"center"} w={300}>
+						<Flex
+							w={40}
+							h={40}
+							color={"white"}
+							rounded={"full"}
+							overflow="hidden"
+							bg={"gray.100"}
+							p={2}
+							pb={0}
+						>
+							<Image src="assets/TheGraphSymbol.png" />
+						</Flex>
+						<Text fontWeight={300}>Graph Advocates</Text>
+					</Stack>
+				</HStack>
+			</VStack>
 
-      <Footer />
-    </>
-  );
+			<Footer />
+		</>
+	);
 }
